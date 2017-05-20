a0ba0b1c5c1eb01a27067285c116e1a03e10e491
bhyang <brianhyang@berkeley.edu>
2017-05-20 02:34:03 -0700

    Fixed a major bug with tupleifying and changed the defaults. Also Chinese example

--- markov.py
+++ markov.py
@@ -1,10 +1,9 @@
 import random, argparse
 from collections import defaultdict
 
-# default char-markov
 class MarkovChain:
 
-	def __init__(self, n):
+	def __init__(self, n=8):
 		self.transition_dict = defaultdict(lambda: [])
 		self.state = ""
 		self.state_size = n
@@ -20,8 +19,8 @@ class MarkovChain:
 		assert self.state_size < len(data), "state_size exceeds the total length of the data"
 		tuplified = []
 		for i in range(len(data) - self.state_size):
-			chunk = ''.join([data[n] for n in range(i, i + self.state_size + 1)])
-			tuplified.append((''.join([data[n] for n in range(i, i + self.state_size)]), data[i + self.state_size]))
+			chunk = tuple([data[n] for n in range(i, i + self.state_size + 1)])
+			tuplified += [(chunk[:-1], chunk[1:])]
 		return tuplified
 
 	# Predicts next word based on current state
@@ -32,10 +31,10 @@ class MarkovChain:
 		else:
 			possible_states = self.transition_dict[self.state]
 		self.state = random.choice(possible_states)
-		return self.state
+		return self.state[-1]
 
 	# Generates a sequence of arbitrary length
-	def generate(self, length, prompt):
+	def generate(self, prompt, length=500):
 		assert length > len(prompt), "Prompt can't be longer than output_length"
 		if self.state_size < len(prompt):	# If the prompt is too small, randomly pick from all states that contain the prompt
 			self.state = random.choice([state for state in self.transition_dict if prompt in state] or self.transition_dict)
@@ -46,9 +45,12 @@ class MarkovChain:
 			return_seq += self.next()
 		return return_seq
 
-punctuation = ['.', ',', '\n', '\"']
+punctuation = ['.', ',', '\n', ':', '?']
 
 class WordMarkovChain(MarkovChain):
+
+	def __init__(self, n=2):
+		MarkovChain.__init__(self, n)
 
 	def tupleify(self, data):
 		assert self.state_size < len(data), "state_size exceeds the total length of the data"
@@ -68,37 +70,47 @@ class WordMarkovChain(MarkovChain):
 				current_word += data[c]
 		word_list += current_word
 
-		tuplified = []
-		for i in range(len(word_list) - self.state_size):
-			chunk = ''.join([word_list[n] for n in range(i, i + self.state_size + 1)])
-			tuplified.append((' '.join([word_list[n] for n in range(i, i + self.state_size)]), word_list[i + self.state_size]))
-		return tuplified
+		return MarkovChain.tupleify(self, word_list)
 
 	# Generates a sequence of arbitrary length
-	def generate(self, length, prompt):
+	def generate(self, prompt, length=100):
 		self.state = prompt
 		return_seq = ""
 		for _ in range(length):
-			nex = self.next()
-			if nex in punctuation:
-				return_seq = return_seq[:-1]
-			if nex != '\n':
-				nex += ' '
-			return_seq += nex
-		return return_seq
+			return_seq += self.next() + ' '
+		return scrub(return_seq)
+
+# Uses some basic guidelines to clean up the generated texts
+def scrub(text):
+	while text[0] in punctuation + [' ']:
+		text = text[1:]
+	delete = []
+	for i in range(len(text)):
+		if text[i - 1] == '\n' and text[i] == ' ':
+			delete += [i]
+		if text[i] in punctuation and text[i - 1] == ' ':
+			delete += [i - 1]
+	return ''.join([text[i] for i in range(len(text)) if i not in delete])
 
 # Constructs Markov chain from command-line arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("--state_size", default=1, type=int, help="Size of each state, measured in either words or characters based on type")
-parser.add_argument("--word_based", default=1, type=int, help="Construct Markov chain using words instead of characters")
-parser.add_argument("--output_length", default=100, type=int, help="Length of generated output, measured in either words of characters based on type")
+parser.add_argument("--state_size", type=int, help="Size of each state, measured in either words or characters based on type")
+parser.add_argument("--word_based", default=0, type=int, help="Construct Markov chain using words instead of characters")
+parser.add_argument("--output_length", type=int, help="Length of generated output, measured in either words of characters based on type")
 parser.add_argument("--input_src", nargs="+", default="data/swift.txt", type=str, help="Path of input file (must be .txt)")
 parser.add_argument("--prompt", default="", type=str, help="User-determined starting state for the Markov chain")
+parser.add_argument("--output_file", type=str, help="Can print the output to a user-specified path")
 args = parser.parse_args()
 
-markov = MarkovChain(args.state_size)
-if args.word_based:
-	markov = WordMarkovChain(args.state_size)
+if args.state_size:
+	markov = MarkovChain(args.state_size)
+	if args.word_based:
+		markov = WordMarkovChain(args.state_size)
+else:
+	markov = MarkovChain()
+	if args.word_based:
+		markov = WordMarkovChain()
+
 if type(args.input_src) == str:
 	markov.update(open(args.input_src).read())
 else:
@@ -106,4 +118,11 @@ else:
 		markov.update(open(src).read())
 
 # Generate stuff
-print(markov.generate(length=args.output_length, prompt=args.prompt))
+if args.output_length:
+	output = markov.generate(length=args.output_length, prompt=args.prompt)
+else:
+	output = markov.generate(prompt=args.prompt)
+if args.output_file:
+	with open(args.output_file, 'w') as dest:
+		dest.write(output)
+print(output)
